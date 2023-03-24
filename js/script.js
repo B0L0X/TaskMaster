d0fb1b4f83ae55279535f31735b80b019cf5bc25
Tomasz Bortkiewicz <xbox001@int.pl>
2023-03-24 19:49:54 +0100

    sections added

--- js/script.js
+++ js/script.js
@@ -1,13 +1,6 @@
 {
     const tasks = [
-        {
-            content: "nagrać lekcję",
-            done: false,
-        },
-        {
-            content: "zjeść pierogi",
-            done: true,
-        },
+
     ];
 
     const addNewTask = (newTaskContent) => {
@@ -51,13 +44,14 @@
 
         for (const task of tasks) {
             htmlString += `
-              <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
-
-              <button class="js-done">zrobione?</button>
-              <button class="js-remove">usuń</button>
-                ${task.content}
-              </li>
-                    `;
+                <li
+                    class="list__item${task.done ? " list__item--done" : ""}"
+                >
+                    <button class="js-done">undone</button>
+                    ${task.content}
+                    <button class="js-remove">del</button>
+                </li>
+            `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
